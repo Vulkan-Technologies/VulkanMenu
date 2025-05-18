475b4da2b1b6928020720e73dfd3f79f2ba37951
Roch Blondiaux <roch.blondiaux@protonmail.com>
2025-05-18 15:40:14 +0200

    feat: itemsadder integration

---
+++ src/main/java/com/vulkantechnologies/menu/hook/implementation/ItemsAdderPluginHook.java
@@ -0,0 +1,50 @@
+package com.vulkantechnologies.menu.hook.implementation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.entity.Player;
+
+import com.vulkantechnologies.menu.VulkanMenu;
+import com.vulkantechnologies.menu.hook.PluginHook;
+import com.vulkantechnologies.menu.model.PlaceholderProcessor;
+
+import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ItemsAdderPluginHook implements PluginHook, PlaceholderProcessor {
+
+    private static final Pattern pattern = Pattern.compile(":(\\w+):");
+    private final VulkanMenu plugin;
+
+    @Override
+    public void onSuccess() {
+        this.plugin.getSLF4JLogger().info("Successfully hooked into ItemsAdder!");
+
+        this.plugin.placeholderProcessors().add(this);
+    }
+
+    @Override
+    public void onFailure() {
+        this.plugin.getSLF4JLogger().warn("Failed to hook into ItemsAdder!");
+        this.plugin.getSLF4JLogger().warn("Some features will be disabled.");
+    }
+
+    @Override
+    public String pluginName() {
+        return "ItemsAdder";
+    }
+
+    @Override
+    public String process(Player player, String content) {
+        Matcher matcher = pattern.matcher(content);
+        StringBuilder result = new StringBuilder();
+        while (matcher.find()) {
+            String replacement = FontImageWrapper.replaceFontImages(matcher.group(0));
+            matcher.appendReplacement(result, replacement);
+        }
+        matcher.appendTail(result);
+        return result.toString();
+    }
+}
