22df03b1825d752870874c251dbcc21693aeca21
Traqueur-dev <traquer.dev@gmail.com>
2025-08-11 12:40:45 +0200

    fix: italic, try empty slot blink, add reload event

--- src/main/java/com/vulkantechnologies/menu/hook/implementation/PacketEventsPluginHook.java
+++ src/main/java/com/vulkantechnologies/menu/hook/implementation/PacketEventsPluginHook.java
@@ -1,11 +1,19 @@
 package com.vulkantechnologies.menu.hook.implementation;
 
 import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetSlot;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerWindowItems;
 import com.vulkantechnologies.menu.VulkanMenu;
 import com.vulkantechnologies.menu.hook.PluginHook;
 import com.vulkantechnologies.menu.listener.packet.InventoryPacketListener;
 
+import com.vulkantechnologies.menu.model.menu.Menu;
+import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 public class PacketEventsPluginHook implements PluginHook {
