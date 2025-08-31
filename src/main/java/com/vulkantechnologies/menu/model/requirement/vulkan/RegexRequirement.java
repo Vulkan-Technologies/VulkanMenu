2d8f4eb2f9fa5870b33cf5639d2e8b5d57ea3666
Roch Blondiaux <roch.blondiaux@protonmail.com>
2025-08-31 14:42:11 +0200

    fix: regex requirement

--- src/main/java/com/vulkantechnologies/menu/model/requirement/vulkan/RegexRequirement.java
+++ src/main/java/com/vulkantechnologies/menu/model/requirement/vulkan/RegexRequirement.java
@@ -6,11 +6,12 @@ import org.bukkit.entity.Player;
 
 import com.vulkantechnologies.menu.VulkanMenu;
 import com.vulkantechnologies.menu.annotation.ComponentName;
+import com.vulkantechnologies.menu.annotation.Single;
 import com.vulkantechnologies.menu.model.menu.Menu;
 import com.vulkantechnologies.menu.model.requirement.Requirement;
 
 @ComponentName("regex")
-public record RegexRequirement(String value) implements Requirement {
+public record RegexRequirement(@Single String regex, String value) implements Requirement {
 
     @Override
     public boolean test(Player player, Menu menu) {
@@ -18,8 +19,8 @@ public record RegexRequirement(String value) implements Requirement {
         String formattedValue = VulkanMenu.get().processPlaceholders(player, menu, value);
 
 
-        Pattern pattern = Pattern.compile(formattedValue);
-        return pattern.matcher(player.getName()).find();
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(formattedValue).find();
     }
 
 }
